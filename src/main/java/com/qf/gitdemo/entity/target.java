6e725d4d044ca8161a2809e6d28a0cf765d963c9
Remi <2911267772@qq.com>
2019-10-23 12:13:51 +0800

    Initial commit

---
+++ src/main/java/com/qf/gitdemo/entity/target.java
@@ -0,0 +1,17 @@
+package com.qf.gitdemo.entity;
+
+/**
+ * @author WangWF
+ * @Date 2019/10/23
+ */
+public class target {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
